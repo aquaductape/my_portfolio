00c8a6fa218648774394aecab26de500f028d178
Caleb Taylor <aquaductape@gmail.com>
2021-07-16 12:46:33 -0700

    update

--- src/components/S_Link/S_Link.tsx
+++ src/components/S_Link/S_Link.tsx
@@ -5,14 +5,15 @@ const S_Link = (props: { children?: JSX.Element }) => {
   const children = props.children as unknown as { t: string };
 
   if (!isBrowser) {
-    // const sLinkStr = '<span data-hk="0.0.0.6.0.2.0" class="s-link"><span class="s-link-gradient"></span><span class="s-link-solid"></span></span>'
-    const sLinkEl = (
-      <span class="s-link">
-        <span class="s-link-gradient"></span>
-        <span class="s-link-solid"></span>
-      </span>
-    );
-    const sLinkStr = (sLinkEl as any).t;
+    const sLinkStr =
+      '<span class="s-link"><span class="s-link-gradient"></span><span class="s-link-solid"></span></span>';
+    // const sLinkEl = (
+    //   <span class="s-link">
+    //     <span class="s-link-gradient"></span>
+    //     <span class="s-link-solid"></span>
+    //   </span>
+    // );
+    // const sLinkStr = (sLinkEl as any).t;
     const childrenStr = children.t as string;
 
     const result = childrenStr.replace(/<a\s.+>(.+)<\/a>/, (_, content) => {
